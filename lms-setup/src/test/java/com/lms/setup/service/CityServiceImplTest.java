91dac925cbee3e5646ed9ed8cd1884339f8dacaa
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-08-30 04:13:43 +0300

    Add OpenTelemetry logback dependency and fix CityServiceImplTest

--- lms-setup/src/test/java/com/lms/setup/service/CityServiceImplTest.java
+++ lms-setup/src/test/java/com/lms/setup/service/CityServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.lms.setup.service;
 
 import com.common.dto.BaseResponse;
+import com.lms.setup.mapper.CityMapper;
 import com.lms.setup.model.City;
 import com.lms.setup.repository.CityRepository;
+import com.lms.setup.repository.CountryRepository;
 import com.lms.setup.service.impl.CityServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,12 +15,14 @@ import org.mockito.MockitoAnnotations;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class CityServiceImplTest {
 
-    @Mock
-    private CityRepository cityRepository;
+    @Mock private CityRepository cityRepository;
+    @Mock private CountryRepository countryRepository;
+    @Mock private CityMapper mapper;
 
     @InjectMocks
     private CityServiceImpl service;
@@ -30,7 +34,11 @@ class CityServiceImplTest {
 
     @Test
     void list_ok() {
-        when(cityRepository.findAll()).thenReturn(Collections.singletonList(new City()));
+        var cityPage = new org.springframework.data.domain.PageImpl<>(Collections.singletonList(new City()));
+        when(cityRepository.findAll(any(org.springframework.data.jpa.domain.Specification.class), any(org.springframework.data.domain.Pageable.class)))
+                .thenReturn(cityPage);
+        when(mapper.toDtoPage(cityPage)).thenReturn(org.springframework.data.domain.Page.empty());
+
         BaseResponse<?> resp = service.list(org.springframework.data.domain.Pageable.unpaged(), null, false);
         assertNotNull(resp);
     }
