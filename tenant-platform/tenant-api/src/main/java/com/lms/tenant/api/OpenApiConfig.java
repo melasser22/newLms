a2bb6d5234b563dd28491c875b159d9d4a7fa441
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-09-01 23:49:53 +0300

    feat: expose policy endpoints and configure openapi

---
+++ tenant-platform/tenant-api/src/main/java/com/lms/tenant/api/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.acme.tenant.api;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .components(new Components())
+        .info(new Info().title("Tenant API").version("1.0.0"));
+  }
+}
