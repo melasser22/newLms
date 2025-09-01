72749f114e71ed14b4c663962f5e332388b5687a
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-09-01 14:26:25 +0300

    Use DTO for country requests and refine caching

---
+++ lms-setup/src/main/java/com/lms/setup/dto/CountryDto.java
@@ -0,0 +1,39 @@
+package com.lms.setup.dto;
+
+import jakarta.validation.constraints.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CountryDto {
+  @Null
+  private Integer countryId;
+
+  @NotBlank @Size(max = 3)
+  private String countryCd;
+
+  @NotBlank @Size(max = 256)
+  private String countryEnNm;
+
+  @NotBlank @Size(max = 256)
+  private String countryArNm;
+
+  @Size(max = 10)
+  private String dialingCode;
+
+  @Size(max = 256)
+  private String nationalityEn;
+
+  @Size(max = 256)
+  private String nationalityAr;
+
+  @NotNull
+  private Boolean isActive;
+
+  @Size(max = 1000)
+  private String enDescription;
+
+  @Size(max = 1000)
+  private String arDescription;
+}
