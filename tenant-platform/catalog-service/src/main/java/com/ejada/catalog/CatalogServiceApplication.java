5c0c81b57118db55a187083d2e2270fd824614b8
EJADA\melasser <melasser@ejada.com>
2025-09-05 00:31:12 +0300

    updated

--- tenant-platform/catalog-service/src/main/java/com/ejada/catalog/CatalogServiceApplication.java
+++ tenant-platform/catalog-service/src/main/java/com/ejada/catalog/CatalogServiceApplication.java
@@ -2,11 +2,12 @@ package com.ejada.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Catalog Service", version = "1.0"))
+@SpringBootApplication(scanBasePackages = "com.ejada")
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
