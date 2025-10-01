dc28a3ef01f1c7f0606ab855131b332160b682de
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-10-02 01:08:23 +0300

    Ensure tenant provisioning properties bean is available

--- tenant-platform/subscription-service/src/main/java/com/ejada/subscription/config/TenantProvisioningConfiguration.java
+++ tenant-platform/subscription-service/src/main/java/com/ejada/subscription/config/TenantProvisioningConfiguration.java
@@ -1,10 +1,16 @@
 package com.ejada.subscription.config;
 
 import com.ejada.common.events.provisioning.TenantProvisioningProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(TenantProvisioningProperties.class)
 public class TenantProvisioningConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.tenant-provisioning")
+    public TenantProvisioningProperties tenantProvisioningProperties() {
+        return new TenantProvisioningProperties();
+    }
 }
