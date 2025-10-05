aff269346ee33d3a0790216973cb22071a7d12aa
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-10-05 03:06:49 +0300

    Configure security repository tests with postgres support

--- sec-service/src/test/java/com/ejada/sec/repository/UserRepositoryTenantIsolationTest.java
+++ sec-service/src/test/java/com/ejada/sec/repository/UserRepositoryTenantIsolationTest.java
@@ -3,11 +3,16 @@ package com.ejada.sec.repository;
 import com.ejada.common.context.ContextManager;
 import com.ejada.common.exception.ValidationException;
 import com.ejada.sec.domain.User;
+import com.ejada.testsupport.extensions.PostgresTestExtension;
+import com.ejada.testsupport.extensions.SecuritySchemaExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,6 +22,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith({PostgresTestExtension.class, SecuritySchemaExtension.class})
+@Testcontainers(disabledWithoutDocker = true)
 class UserRepositoryTenantIsolationTest {
 
     @Autowired
