0b71ecde8e49790fccf71698a264763734209e68
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-11-12 13:08:01 +0300

    Scan tenant config properties

--- tenant-platform/tenant-service/src/main/java/com/ejada/tenant/TenantApplication.java
+++ tenant-platform/tenant-service/src/main/java/com/ejada/tenant/TenantApplication.java
@@ -2,11 +2,13 @@ package com.ejada.tenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.ejada.tenant")
 @EnableCaching
 @OpenAPIDefinition(info = @Info(title = "Ejada Tenant Service", version = "1.0"))
 public class TenantApplication {
