89d38140d2e0a5ca8e07e972c9d8be9021f52db1
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-11-16 23:43:31 +0300

    Add unit tests for child services

---
+++ email-management/email-management-service/src/test/java/com/ejada/management/config/ChildServicePropertiesTest.java
@@ -0,0 +1,39 @@
+package com.ejada.management.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+class ChildServicePropertiesTest {
+
+  @Test
+  void getTemplateShouldReturnDefensiveCopy() {
+    ChildServiceProperties properties = new ChildServiceProperties();
+    ChildServiceProperties.ServiceEndpoint internalTemplate =
+        (ChildServiceProperties.ServiceEndpoint) ReflectionTestUtils.getField(properties, "template");
+    URI templateUri = URI.create("https://template.test");
+    ReflectionTestUtils.setField(internalTemplate, "baseUrl", templateUri);
+
+    ChildServiceProperties.ServiceEndpoint retrievedTemplate = properties.getTemplate();
+
+    assertThat(retrievedTemplate).isNotSameAs(internalTemplate);
+    assertThat(retrievedTemplate.getBaseUrl()).isEqualTo(templateUri);
+
+    retrievedTemplate.setBaseUrl(URI.create("https://mutated.test"));
+    ChildServiceProperties.ServiceEndpoint secondRead = properties.getTemplate();
+
+    assertThat(secondRead.getBaseUrl()).isEqualTo(templateUri);
+  }
+
+  @Test
+  void settersShouldUpdateBaseUrl() {
+    ChildServiceProperties.ServiceEndpoint endpoint = new ChildServiceProperties.ServiceEndpoint();
+    URI baseUrl = URI.create("https://child.test/api");
+
+    endpoint.setBaseUrl(baseUrl);
+
+    assertThat(endpoint.getBaseUrl()).isEqualTo(baseUrl);
+  }
+}
