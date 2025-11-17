f32d547c7e2f698e6c02c4947ce332e7aba949f3
melasser22 <148053917+melasser22@users.noreply.github.com>
2025-11-17 15:30:51 +0300

    Enforce tenant scoped queries in template service

--- email-management/email-template-service/src/main/java/com/ejada/email/template/repository/EmailSendRepository.java
+++ email-management/email-template-service/src/main/java/com/ejada/email/template/repository/EmailSendRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailSendRepository extends JpaRepository<EmailSendEntity, Long> {
   Optional<EmailSendEntity> findByIdempotencyKey(String idempotencyKey);
+
+  Optional<EmailSendEntity> findByIdAndTenantId(Long id, String tenantId);
 }
